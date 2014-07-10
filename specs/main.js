01574ff4243b1d8e095656e1919eab0d87ee1bff
Andrew Vorobyov <vorandrew@gmail.com>
2014-07-10 21:16:36 +0300

    checkSymbol

--- specs/main.js
+++ specs/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var RippleBonds = require('../lib/ripple-bonds.js');
 
 describe('Global', function () {
@@ -19,15 +20,31 @@ describe('Global', function () {
 
 describe('Symbol definition', function () {
 
-  it('Correct month code', function () {
-    expect(RippleBonds.monthCodes).toBeDefined();
+  var incorrectCodes = ['XWZ', 'EXU'];
+
+  var correctCodes = ['XFF', 'IDF'];
+
+  it('Checking month code - must throw', function () {
+
+    _(incorrectCodes).forEach(function (elm) {
+
+      var f = function () {
+        RippleBonds.checkSymbol(elm);
+      };
+
+      expect(f).toThrow();
+    });
   });
 
-  it('Has year codes', function () {
-    expect(RippleBonds.yearCodes).toBeDefined();
-  });
+  it('Checking month code - must not throw', function () {
 
-  it('Has currency codes', function () {
-    expect(RippleBonds.currencyCodes).toBeDefined();
+    _(correctCodes).forEach(function (elm) {
+
+      var f = function () {
+        RippleBonds.checkSymbol(elm);
+      };
+
+      expect(f).not.toThrow();
+    });
   });
 });
